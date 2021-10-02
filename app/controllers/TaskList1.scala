097dc9a8ca92a906ca7153a491d54e5be12966ad
eren <eozel44@gmail.com>
2021-10-02 15:47:37 +0300

    login form with get

--- app/controllers/TaskList1.scala
+++ app/controllers/TaskList1.scala
@@ -7,6 +7,16 @@ import play.api.mvc._
 @Singleton
 class TaskList1 @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+
+  def login=Action{
+    Ok(views.html.login1())
+  }
+
+  def validateLoginGet(username:String,password:String)=Action{
+
+    Ok(s"username: $username logged in with $password")
+  }
+
   def taskList = Action{
     val tasks = List("task 1","task 2","task 3")
     Ok(views.html.TaskList1(tasks))
