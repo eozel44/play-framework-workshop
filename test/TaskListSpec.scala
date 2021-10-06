3c342638059c107c89dff9dbadb34abdf3289668
eren <eozel44@gmail.com>
2021-10-06 20:29:10 +0300

    play controller test with selenium api

--- test/TaskListSpec.scala
+++ test/TaskListSpec.scala
@@ -1,9 +1,31 @@
 import org.scalatestplus.play.PlaySpec
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import org.scalatestplus.play.OneBrowserPerSuite
+import org.scalatestplus.play.HtmlUnitFactory
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 
-class TaskListSpec extends PlaySpec with GuiceOneAppPerSuite with OneBrowserPerSuite {
+class TaskListSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory{
+    "TaskList" must{
 
+        "login and see task list" in {
+      go to s"http://localhost:$port/login"
+      pageTitle mustBe "Login"
+      find(cssSelector("h2")).isEmpty mustBe false
+      find(cssSelector("h2")).foreach(e => e.text mustBe "Login")
+      click on "username-login"
+      textField("username-login").value = "eren"
+      click on "password-login"
+      pwdField(id("password-login")).value = "123"
+      submit()
+      eventually {
+        pageTitle mustBe "Task List"
+        find(cssSelector("h2")).isEmpty mustBe false
+        find(cssSelector("h2")).foreach(e => e.text mustBe "Task List")
+        findAll(cssSelector("li")).toList.map(_.text) mustBe List("eat", "sleep", "code")
+      }
+    }
+
+
+    }
 
 
 }
