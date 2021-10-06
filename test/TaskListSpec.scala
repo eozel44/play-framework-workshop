db09e862c9a252c7d50ba8c94bd71675a33b356a
eren <eozel44@gmail.com>
2021-10-06 15:51:50 +0300

    play test controller

---
+++ test/TaskListSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import org.scalatestplus.play.OneBrowserPerSuite
+
+class TaskListSpec extends PlaySpec with GuiceOneAppPerSuite with OneBrowserPerSuite {
+
+
+
+}
