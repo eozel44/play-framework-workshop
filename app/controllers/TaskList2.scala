385b568d2e85592d9c54b5798e53e509b3327f72
eren <eozel44@gmail.com>
2021-10-07 10:51:15 +0300

    play jquery tasklist2

--- app/controllers/TaskList2.scala
+++ app/controllers/TaskList2.scala
@@ -9,7 +9,11 @@ import models.TaskListInMemoryModel
 class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
     def load=Action{ implicit request=>
-        Ok(views.html.version2Main())
+      request.session.get("username").map { username =>              
+        Ok(views.html.tasklist2(TaskListInMemoryModel.getTasks(username)))
+      }.getOrElse(Ok(views.html.version2Main()))
+        
+        
     }
 
     def login=Action{ implicit request=>
@@ -22,5 +26,33 @@ class TaskList2 @Inject()(cc: ControllerComponents) extends AbstractController(c
       else
         Ok(views.html.login2())
     }
+
+    def create(username:String,password:String)=Action{ implicit request=>
+        
+        if (TaskListInMemoryModel.createUser(username, password))
+        Ok(views.html.tasklist2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+      else
+        Ok(views.html.login2())
+    }
+
+    def deleteTask(index:String)=Action{ implicit request=>
+      request.session.get("username").map { username=>
+        if (TaskListInMemoryModel.removeTask(username,index.toInt))
+        Ok(views.html.tasklist2(TaskListInMemoryModel.getTasks(username))).withSession("username" -> username)
+      else
+        Ok(views.html.login2())
+      }.getOrElse(Redirect(routes.TaskList2.login))
+    }
+
+    def addTask(task:String) = Action { implicit request =>
+    request.session.get("username").map { username =>              
+        TaskListInMemoryModel.addTask(username, task)
+        Ok(views.html.tasklist2(TaskListInMemoryModel.getTasks(username)))    
+    }.getOrElse(Redirect(routes.TaskList2.login))
+  }
+
+   def logout = Action {
+    Redirect(routes.TaskList2.load).withNewSession
+  }
 
 }
--- public/javascripts/version2.js
+++ public/javascripts/version2.js
@@ -3,11 +3,20 @@ $("#contents").load("/login2")
 function login() {
 	const username = $("#loginName").val();     
 	const password = $("#loginPass").val();
-    $("#contents").load("/validate2?username="+username+"&password="+password)
+    $("#contents").load("/validate2?username="+username+"&password="+password);
 }
 
 function createUser() {
-            const username = $("#createName").val();
-            const password = $("#createPass").val();
-    console.log(username)
+    const username = $("#createName").val();
+    const password = $("#createPass").val();
+    $("#contents").load("/create2?username="+username+"&password="+password);
+}
+
+function addTask() {
+    const task = $("#newTask").val();
+    $("#contents").load("/addTask2?task="+encodeURIComponent(task));
+}
+
+function deleteTask(index){
+    $("#contents").load("/deleteTask2?index="+index);
 }
